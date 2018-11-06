ffcba3d4671f5cbd91a964c3d133f80c7584fb5b
lixz <lixz@dns.com>
2018-11-06 14:14:27 +0800

    cookie get set del

--- bindata/example_project/src/utils/cookie.js
+++ bindata/example_project/src/utils/cookie.js
@@ -13,3 +13,15 @@ export function setToken(token) {
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
+
+export function Get(key) {
+  return Cookies.get(key)
+}
+
+export function Set(key, value) {
+  return Cookies.set(key, value)
+}
+
+export function Del(key) {
+  return Cookies.remove(key)
+}
