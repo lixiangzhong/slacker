08b634dc1ec780edd6926c4f4b7242411bbd2acf
lixz <lixz@dns.com>
2018-11-15 14:27:13 +0800

    transition

--- bindata/example_project/src/layout/AppMain.vue
+++ bindata/example_project/src/layout/AppMain.vue
@@ -15,7 +15,7 @@
 <style scoped>
     .slide-fade-enter-active,
     .slide-fade-leave-active {
-      transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+      transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1);
     }
     .slide-fade-enter,
     .slide-fade-leave-to {
