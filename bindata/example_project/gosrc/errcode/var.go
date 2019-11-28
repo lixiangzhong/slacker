f090995966a99a81797b5c5e82261f42237e2c97
lixz <lixz@dns.com>
2019-11-28 16:51:04 +0800

    errcode add

--- bindata/example_project/gosrc/errcode/var.go
+++ bindata/example_project/gosrc/errcode/var.go
@@ -1,13 +1,20 @@
 package errcode
 
 var (
-	OK                 = add(0, "ok")
-	BadRequest         = add(1, "参数错误")
-	Permission         = add(2, "没有操作权限")
-	InvalidCaptcha     = add(3, "验证码错误")
-	NotFound           = add(20, "数据不存在")
-	AlreadyExist       = add(21, "数据已经存在")
-	IncorrectUserOrPwd = add(400, "账号或密码不正确") //401-499  登录相关,前端可控制跳转重新登录
-	InvalidToken       = add(403, "登录信息失效,请重新登录")
-	Error              = add(9999, "错误")
+	OK                       = add(0, "ok")
+	BadRequest               = add(1, "参数错误")
+	Permission               = add(2, "没有操作权限")
+	InvalidCaptcha           = add(3, "验证码错误")
+	Empty                    = add(4, "不能为空")
+	NotFound                 = add(20, "数据不存在")
+	AlreadyExist             = add(21, "数据已经存在")
+	IncorrectUserOrPwd       = add(400, "账号或密码不正确") //401-499  登录相关,前端可控制跳转重新登录
+	InvalidToken             = add(403, "登录信息失效,请重新登录")
+	SQLAffectedEq0           = add(1012, "操作失败:0")
+	AdminLocked              = add(1013, "管理锁定,无法操作")
+	InvalidResource          = add(1015, "不正确的资源")
+	TooMany                  = add(1020, "数量限制")
+	TemporaryNoDataAvailable = add(1021, "暂无可用数据")
+	RateLimit                = add(1111, "频率限制")
+	Error                    = add(9999, "错误")
 )
