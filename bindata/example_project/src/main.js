07a148fd5553a9c3a79d8ec77dbf0fa1243fe320
lixz <lixz@dns.com>
2019-11-28 17:06:18 +0800

    Vue.filter

--- bindata/example_project/src/main.js
+++ bindata/example_project/src/main.js
@@ -39,8 +39,33 @@ Vue.filter("int2ip", function(i) {
   return utils.int2ip(i);
 });
 
+Vue.filter("int2ip_reverse", function(i) {
+  return utils.int2ip_reverse(i);
+});
+
 Vue.filter("unix2datetime", function(i) {
   return utils.parseTime(i);
+});
+
+Vue.filter("Kbyte", function(i) {
+  if (i == NaN || i == undefined) {
+    return "0";
+  }
+  return utils.formatKbps(i * 8);
+});
+
+Vue.filter("duration", function(i) {
+  if (i == NaN || i == undefined) {
+    return "0秒";
+  }
+  var day = Math.floor(i / (24 * 3600)); // Math.floor()向下取整
+  var hour = Math.floor((i - day * 24 * 3600) / 3600);
+  var minute = Math.floor((i - day * 24 * 3600 - hour * 3600) / 60);
+  var second = i - day * 24 * 3600 - hour * 3600 - minute * 60;
+  if (day) {
+    return day + "天" + hour + "时" + minute + "分" + second + "秒";
+  }
+  return hour + "时" + minute + "分" + second + "秒";
 });
 
 new Vue({
